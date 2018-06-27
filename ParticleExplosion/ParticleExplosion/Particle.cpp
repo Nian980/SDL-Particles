f4ddda4c34422999a4c2854c74b66c42f353435e
Nianxu <nianxuwa@usc.edu>
2018-06-27 21:34:02 +0800

    BEFORE Polar Coordinates change
    Cartesian coordinates system.
    Particles instantiate at random locations with random speed. Does not go out of bound.
    Also changes color gradually.

--- ParticleExplosion/ParticleExplosion/Particle.cpp
+++ ParticleExplosion/ParticleExplosion/Particle.cpp
@@ -11,17 +11,17 @@
 
 namespace nian {
     
-    Particle::Particle() : mX(0), mY(0) {
-        /* old stuff
+    Particle::Particle() // : mX(0), mY(0)
+    {
+        //old stuff
         //create particles in a 1 by 1 space, then mapping to the screen
         //rand() gives number between 0 and RAND_MAX, so /RAND_MAX gives 0 to 1, *2.0 gives 0 to 2.0, -1 gives -1 to 1
         mX = (2.0 * rand())/RAND_MAX - 1;
         mY = (2.0 * rand())/RAND_MAX - 1;
         xSpeed = 0.001 * ((2.0 * rand())/RAND_MAX - 1); //get a double value -1 to 1
         ySpeed = 0.001 * ((2.0 * rand())/RAND_MAX - 1);
-        */
         
-        mDirection = (double)rand()/RAND_MAX;
+        //mDirection = (double)rand()/RAND_MAX;
     }
 
     Particle::~Particle() {
@@ -29,7 +29,7 @@ namespace nian {
     }
     
     void Particle::update() {
-        /* Old stuff.
+        //Old stuff.
         mX += xSpeed;
         mY += ySpeed;
         if (mX < -1.0 || mX > 1.0) {
@@ -38,6 +38,6 @@ namespace nian {
         if (mY < -1.0 || mY > 1.0) {
             ySpeed *= -1; //flip speed if going outside of space
         }
-         */
+
     }
 }
